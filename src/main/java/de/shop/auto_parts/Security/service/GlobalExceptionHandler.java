6dd94c58a0ac7f7a057cc60219359612581a4332
Oleksandr <oyehiazarian@united-internet.de>
2024-10-07 13:40:00 +0200

    chore: added cors config

--- src/main/java/de/shop/auto_parts/Security/service/GlobalExceptionHandler.java
+++ src/main/java/de/shop/auto_parts/Security/service/GlobalExceptionHandler.java
@@ -1,5 +1,4 @@
 package de.shop.auto_parts.Security.service;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
